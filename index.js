85dae8812e9cc4bde9b6d405cdc8825ff577e5f0
Athif binu <binuathif@gmail.com>
2022-07-12 12:24:05 +0530

    To comleat The Project

---
+++ index.js
@@ -0,0 +1,19 @@
+
+
+const screen = document.getElementById('screen');
+
+
+function Btnclick(value){
+  screen.value+=value;        //it is used to display a continus number on screen
+    
+
+}
+
+function ClearScreen(){
+     screen.value="";        // to clear the screen 
+}
+
+function FindResult(){
+    const result = eval(screen.value) 
+    screen.value=result;
+}
